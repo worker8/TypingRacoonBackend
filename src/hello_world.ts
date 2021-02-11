bdbe20fdd3677f7a550414d498d271671051ce96
Tan Jun Rong <tanjunrong@gmail.com>
2021-02-12 00:25:56 +0900

    fix port for heroku

--- src/hello_world.ts
+++ src/hello_world.ts
@@ -71,7 +71,7 @@ app.get("/", (req, res) => {
 });
 
 // start the express server
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
     // tslint:disable-next-line:no-console
     console.log(`server started at http://localhost:${port}`);
 });
